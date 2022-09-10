ac77d49b7360ab79dc0d8076fb8aea550949cad9
dev-yongjaelee <dev.yongjaelee@cakecorp.com>
2022-09-10 17:58:27 +0900

    fix: 뒤로가기 정상화되도록 처리한다

--- src/component/WebViewWrapper.tsx
+++ src/component/WebViewWrapper.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
+import {BackHandler} from 'react-native';
 import {WebView, WebViewProps} from 'react-native-webview';
 
 interface WebViewWrapperProps extends WebViewProps {
@@ -6,8 +7,33 @@ interface WebViewWrapperProps extends WebViewProps {
 }
 
 const WebViewWrapper = ({uri, ...restProps}: WebViewWrapperProps) => {
+  const webview = useRef();
+  const [isCanGoBack, setIsCanGoBack] = useState(false);
+  const onPressHardwareBackButton = () => {
+    if (webview.current && isCanGoBack) {
+      webview.current.goBack();
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener(
+      'hardwareBackPress',
+      onPressHardwareBackButton,
+    );
+    return () => {
+      BackHandler.removeEventListener(
+        'hardwareBackPress',
+        onPressHardwareBackButton,
+      );
+    };
+  }, [isCanGoBack]);
+
   return (
     <WebView
+      ref={webview}
       pullToRefreshEnabled={true}
       startInLoadingState={true}
       allowsBackForwardNavigationGestures={true}
@@ -15,11 +41,30 @@ const WebViewWrapper = ({uri, ...restProps}: WebViewWrapperProps) => {
       mixedContentMode={'compatibility'}
       originWhitelist={['https://*', 'http://*']}
       overScrollMode={'never'}
-      injectedJavaScript={
-        " (function() { function wrap(fn) { return function wrapper() { var res = fn.apply(this, arguments); window.ReactNativeWebView.postMessage(window.location.href); return res; } } history.pushState = wrap(history.pushState); history.replaceState = wrap(history.replaceState); window.addEventListener('popstate', function() { window.ReactNativeWebView.postMessage(window.location.href); }); })(); true; "
-      }
-      onMessage={event => {
-        console.log('onMessage', event.nativeEvent.data);
+      injectedJavaScript={`
+        (function() {
+          function wrap(fn) {
+            return function wrapper() {
+              var res = fn.apply(this, arguments);
+              window.ReactNativeWebView.postMessage('navigationStateChange');
+              return res;
+            }
+          }
+    
+          history.pushState = wrap(history.pushState);
+          history.replaceState = wrap(history.replaceState);
+          window.addEventListener('popstate', function() {
+            window.ReactNativeWebView.postMessage('navigationStateChange');
+          });
+        })();
+    
+        true;
+      `}
+      onMessage={({nativeEvent: state}) => {
+        if (state.data === 'navigationStateChange') {
+          // Navigation state updated, can check state.canGoBack, etc.
+          setIsCanGoBack(state.canGoBack);
+        }
       }}
       {...restProps}
     />
